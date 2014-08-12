2bf676ba92c500fd9571ab98c90ee4b4c3547e02
Dhavide Aruliah <dhavide@gmail.com>
2014-08-12 12:44:29 -0400

    Cleaned up src directory; moved tests directory

--- src/assign_depths.c
+++
@@ -1,9 +0,0 @@
-#include "pampac.h"
-void assign_depths (PTnode *alpha, int depth) {
-  for (int k=0;  k<alpha->max_children; k++) {
-    PTnode *beta = alpha->child[k];
-    if (beta != NULL)
-      assign_depths (beta, depth+1 );
-  }
-  alpha->depth = depth;
-}
