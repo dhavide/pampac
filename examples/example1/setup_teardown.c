d3d41ee3a2892b0e837f1bf3e7b796bc53e9321e
Dhavide Aruliah <dhavide@gmail.com>
2014-08-13 01:24:01 -0400

    Removing unneeded file

--- examples/example1/setup_teardown.c
+++
@@ -1,8 +0,0 @@
-#include "KS_example.h"
-
-void setup_teardown(int N) {
-  // Required to create or destroy FFT plans for both routines
-  compute_residual(N, NULL, NULL);
-  single_corrector_step(N, NULL, NULL);
-  return;
-}
